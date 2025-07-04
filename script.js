d0c978cf6f47114090c819739058e1676fc3a215
bruno-src <millermulumba@gmail.com>
2025-07-04 20:26:52 +0200

    removing script

--- script.js
+++
@@ -1,9 +0,0 @@
-function sendMessage(){
-    let parms = {
-        name:document.getElementById("name-field").value,
-        email:document.getElementById("email-field").value,
-        subject:document.getElementById("subject-field").value,
-        message:document.getElementById("message-field").value,
-    }
-    emailjs.send("service_9di05ss","template_ffeheq8",parms).then(alert("Email sent !!"))
-}
